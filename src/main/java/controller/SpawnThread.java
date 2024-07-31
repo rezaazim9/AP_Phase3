05cc586a5a3d3f3e6b3536ac6e0726ba5e87d00a
rezaazim9 <ostadrezaazim@gmail.com>
2024-07-31 16:59:18 +0330

    first

---
+++ src/main/java/controller/SpawnThread.java
@@ -0,0 +1,89 @@
+package controller;
+
+
+import model.WaveManager;
+import model.characters.*;
+import model.movement.Direction;
+import model.movement.Movable;
+
+import java.awt.*;
+
+import static controller.AudioHandler.random;
+import static controller.UserInterfaceController.eliminateView;
+import static controller.UserInterfaceController.getMainMotionPanelId;
+import static controller.constants.WaveConstants.MAX_ENEMY_SPAWN_RADIUS;
+import static controller.constants.WaveConstants.MIN_ENEMY_SPAWN_RADIUS;
+import static model.Utils.*;
+import static model.WaveManager.waveEntities;
+import static model.characters.GeoShapeModel.allShapeModelsList;
+import static model.collision.Collidable.collidables;
+
+public class SpawnThread extends Thread {
+    private int wave;
+    private static SpawnThread spawnThread = new SpawnThread();
+
+
+    public void setWave(int wave) {
+        this.wave = wave;
+    }
+
+    public static SpawnThread getSpawnThread() {
+        return spawnThread;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    private boolean running = true;
+
+    int i = 0;
+
+    @Override
+    public void run() {
+       k: while (true) {
+            while (running) {
+                if (WaveManager.wave > 5) {
+                    for (GeoShapeModel shapeModel : waveEntities) {
+                        allShapeModelsList.remove(shapeModel);
+                        collidables.remove(shapeModel);
+                        Movable.movables.remove(shapeModel);
+                        eliminateView(shapeModel.getModelId(), shapeModel.getMotionPanelId());
+                    }
+                    waveEntities.clear();
+                    break k;
+                }
+                try {
+                    sleep((int) (3500 / (Math.pow(wave + 1, 0.2))));
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+                Point location = roundPoint(addUpPoints(EpsilonModel.getINSTANCE().getAnchor(),
+                        multiplyPoint(new Direction(random.nextFloat(0, 360)).getDirectionVector(),
+                                random.nextFloat(MIN_ENEMY_SPAWN_RADIUS.getValue(), MAX_ENEMY_SPAWN_RADIUS.getValue()))));
+                GeoShapeModel model;
+                if (wave == 0) {
+                    model = new SquarantineModel(location, getMainMotionPanelId());
+                } else {
+                    model = switch (random.nextInt(0, 2)) {
+                        case 0 -> new SquarantineModel(location, getMainMotionPanelId());
+                        case 1 -> new TrigorathModel(location, getMainMotionPanelId());
+                        default -> null;
+                    };
+                }
+                if (model != null) {
+                    WaveManager.waveEntities.add(model);
+                    model.getMovement().lockOnTarget(EpsilonModel.getINSTANCE().getModelId());
+                    System.out.println(++i);
+                }
+
+            }
+            try {
+                sleep( 400);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+}
